7cbc3bd4fb7cebd4c461ce366eb3f51cb3f4bd70
yzkim9501 <yzkim9501@naver.com>
2021-06-19 14:24:08 +0900

    Create 11053.py

---
+++ 11053.py
@@ -0,0 +1,13 @@
+# 수열 A가 주어졌을 때, 가장 긴 증가하는 부분 수열을 구하는 프로그램을 작성하시오.
+
+#예를 들어, 수열 A = {10, 20, 10, 30, 20, 50} 인 경우에 가장 긴 증가하는 부분 수열은 {10, 20, 30, 50} 이고, 길이는 4이다.
+
+n = int(input())
+a = list(map(int, input().split()))
+dp = [0 for i in range(n)]
+for i in range(n):
+    for j in range(i):
+        if a[i] > a[j] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+    dp[i] += 1
+print(max(dp))
